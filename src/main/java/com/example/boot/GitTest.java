3c1a4e6752d266f036ab83aec27c62dda19ed408
yuanliang <1209715660@qq.com>
2022-08-16 17:07:35 +0800

    first commit

---
+++ src/main/java/com/example/boot/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.boot;
+
+/**
+ * @author yl
+ * @create 2022-08-16 16:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
